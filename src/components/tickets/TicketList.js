40d6e62656dc7f8a7162524ed1cc0c4c4c729f6a
mlwheaton08 <mlwheaton06@gmail.com>
2022-10-15 13:16:49 -0500

    part 5

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -3,6 +3,10 @@ import "./Tickets.css"
 
 export const TicketList = () => {
     const [tickets, setTickets] = useState([])
+    const [filteredTickets, setFiltered] = useState([])
+
+    const localHoneryUser = localStorage.getItem("honey_user")
+    const honeyUserObject = JSON.parse(localHoneryUser)
 
     useEffect(
         () => {
@@ -17,13 +21,25 @@ export const TicketList = () => {
         [] // When this array is empty, you are observing initial component state
     )
 
+    useEffect(
+      () => {
+        if (honeyUserObject.staff) {
+          setFiltered(tickets)
+        } else {
+          const myTickets = tickets.filter(ticket => ticket.userId === honeyUserObject.id)
+          setFiltered(myTickets)
+        }
+      },
+      [tickets]
+    )
+
     return <>
       <h2>List of Tickets</h2>
       <article className="tickets">
         {
-          tickets.map(
+          filteredTickets.map(
             (ticket) => {
-              return <section className="ticket">
+              return <section key={ticket.id} className="ticket">
                 <header>{ticket.description}</header>
                 <footer>Emergency: {ticket.emergency ? "❗" : "No" }</footer>
               </section>
