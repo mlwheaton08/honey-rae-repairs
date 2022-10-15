87a44de1b6c275c38801918027999eeb0554320e
mlwheaton08 <mlwheaton06@gmail.com>
2022-10-15 12:45:09 -0500

    part 4

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react"
+import "./Tickets.css"
 
 export const TicketList = () => {
     const [tickets, setTickets] = useState([])
 
     useEffect(
         () => {
+            const fetchData = async () => {
+                const response = await fetch(`http://localhost:8088/serviceTickets`)
+                const ticketArray = await response.json()
+                setTickets(ticketArray)
+            }
+            fetchData()
             console.log("Initial state of tickets", tickets) // View the initial state of tickets
         },
         [] // When this array is empty, you are observing initial component state
     )
-    return <h2>List of Tickets</h2>
+
+    return <>
+      <h2>List of Tickets</h2>
+      <article className="tickets">
+        {
+          tickets.map(
+            (ticket) => {
+              return <section className="ticket">
+                <header>{ticket.description}</header>
+                <footer>Emergency: {ticket.emergency ? "❗" : "No" }</footer>
+              </section>
+            }
+          )
+        }
+      </article>
+    </>
 }
