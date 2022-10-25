21d58c7903ca24430259ef8077598c6b9174ac1d
mlwheaton08 <mlwheaton06@gmail.com>
2022-10-25 18:41:29 -0500

    first commit. part 9 complete

--- src/components/tickets/TicketList.js
+++ src/components/tickets/TicketList.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import "./Tickets.css"
 
-export const TicketList = () => {
+export const TicketList = ({searchTermState}) => {
     const [tickets, setTickets] = useState([])
     const [filteredTickets, setFilteredTickets] = useState([])
     const [emergency, setEmergency] = useState(false)
@@ -11,6 +11,16 @@ export const TicketList = () => {
 
     const localHoneryUser = localStorage.getItem("honey_user")
     const honeyUserObject = JSON.parse(localHoneryUser)
+
+    useEffect(
+      () => {
+        const searchedTickets = tickets.filter(ticket => {
+          return ticket.description.toLowerCase().startsWith(searchTermState.toLowerCase())
+        })
+        setFilteredTickets(searchedTickets)
+      },
+      [searchTermState]
+    )
 
     useEffect(
       () => {
